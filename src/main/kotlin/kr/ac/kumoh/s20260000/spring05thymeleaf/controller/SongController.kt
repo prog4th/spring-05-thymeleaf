05b4f1545a576cc956d987c182e90d1f9de01be0
prog4th <prog4th@proton.me>
2026-01-10 07:02:18 +0900

    🚧Tyhmeleaf에서 model 사용

--- src/main/kotlin/kr/ac/kumoh/s20260000/spring05thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring05thymeleaf/controller/SongController.kt
@@ -1,12 +1,17 @@
 package kr.ac.kumoh.s20260000.spring05thymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/songs/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model: Model): String {
+        model.addAttribute(
+            "songTitle",
+            "Soda Pop"
+        )
         return "random"
     }
 }

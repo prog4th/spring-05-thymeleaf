5538916744ac993f85d2e2762907c3028ae299dc
prog4th <prog4th@proton.me>
2026-01-10 06:42:38 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring05thymeleaf/Spring05ThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20260000.spring05thymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring05ThymeleafApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring05ThymeleafApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20260000/spring05thymeleaf/Spring05ThymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20260000.spring05thymeleaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring05ThymeleafApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
